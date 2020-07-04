673d2d83b966bac6a9b980f266ee51e6376350cb
RayDarar <dfqgth400@gmail.com>
2020-07-04 15:49:37 +0600

    basic configuration

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div></div>
+  <div id="app">
+    <header class="header"></header>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+  border: none;
+  box-sizing: border-box;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
