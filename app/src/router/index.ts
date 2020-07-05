1648c9ee34ea5f02255a0e5eb96dbd1ab6d450c2
RayDarar <dfqgth400@gmail.com>
2020-07-05 22:04:59 +0600

    feature: not found case

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -10,6 +10,14 @@ const routes: Array<RouteConfig> = [
     path: "/",
     component: MainPage,
   },
+  {
+    path: "/form/:worker_id",
+    component: () => import("@/views/WorkerForm/WorkerForm.vue"),
+  },
+  {
+    path: "*",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ app/src/views/NotFound.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="not-found">
+    <span class="label">Не Найдено</span>
+    <base-button class="buton" @click="goBack">Назад</base-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.back();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#not-found {
+  @extend .column-container;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  font: {
+    size: 2.5rem;
+    family: $fontTitle;
+  }
+}
+.label {
+  margin-bottom: 0.5em;
+}
+</style>
